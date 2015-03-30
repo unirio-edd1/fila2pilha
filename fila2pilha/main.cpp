caa36421e8e3355e81ed15a028011ec3800a5fcf
diogommartins <diogo.martins@unirio.br>
2015-03-30 18:24:39 -0300

    Initial Commit

---
+++ fila2pilha/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fila2pilha
+//
+//  Created by Diogo Martins on 3/30/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
